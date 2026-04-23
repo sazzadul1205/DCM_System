6d5f2d0d214c560142be5ef3892f96522acbb87b
Sazzadul Islam <psazzadul@gmail.com>
2026-04-23 13:59:01 +0600

    - Updated a Migration -  Removed unused Pages

--- database/migrations/2026_04_22_134060_create_users_table.php
+++ database/migrations/2026_04_22_134060_create_users_table.php
@@ -35,6 +35,15 @@ return new class extends Migration
             $table->index('deleted_at');
             $table->index(['id', 'deleted_at']);
         });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     public function down(): void
